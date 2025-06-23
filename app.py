fcaa4d2b653662df134caabfcd7f6166ef384411
Sam Prince <sam@Sams-MacBook-Pro.local>
2025-06-22 21:15:21 -0500

    updated the origins for pre-deploy testing

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://www.basemshaker.com", "http://localhost:5500"],
+    allow_origins = ["https://www.basemshaker.com", "http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
