4220c42846c2f3952b2a4342274027b45e9f6d58
Sam Prince <sam@Sams-MacBook-Pro.local>
2025-06-22 20:30:34 -0500

    updated the origin to the website

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, replace with your domain
+    allow_origins=["https://www.basemshaker.com"],  # In production, replace with your domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
