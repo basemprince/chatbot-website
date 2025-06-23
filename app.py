5b6c525543a4bce3439ee8895fe5f1acab669631
Sam Prince <sam@Sams-MacBook-Pro.local>
2025-06-22 20:58:21 -0500

    fixed the bug

--- app.py
+++ app.py
@@ -18,10 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = [
-    "https://www.basemshaker.com",
-    "http://localhost:5500", # for local development
-    ]
+    allow_origins = ["https://www.basemshaker.com", "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
